263a2243ff5dc4e474d4e603d6bf86ddc9d49196
Daniel <daniel.jaek@gmail.com>
2022-07-01 17:12:07 +0200

    fix import

--- bungio/models/basic/character.py
+++ bungio/models/basic/character.py
@@ -1,11 +1,15 @@
+from typing import TYPE_CHECKING
+
 import attr
 
-from bungio.models import BungieMembershipType
 from bungio.models.mixins import DestinyCharacterMixin
+
+if TYPE_CHECKING:
+    from bungio.models import BungieMembershipType
 
 
 @attr.define
 class DestinyCharacter(DestinyCharacterMixin):
     membership_id: int = attr.field()
-    membership_type: BungieMembershipType = attr.field()
+    membership_type: "BungieMembershipType" = attr.field()
     character_id: int = attr.field()
--- bungio/models/basic/user.py
+++ bungio/models/basic/user.py
@@ -1,10 +1,14 @@
+from typing import TYPE_CHECKING
+
 import attr
 
-from bungio.models import BungieMembershipType
 from bungio.models.mixins import DestinyUserMixin
+
+if TYPE_CHECKING:
+    from bungio.models import BungieMembershipType
 
 
 @attr.define
 class DestinyUser(DestinyUserMixin):
     membership_id: int = attr.field()
-    membership_type: BungieMembershipType = attr.field()
+    membership_type: "BungieMembershipType" = attr.field()
